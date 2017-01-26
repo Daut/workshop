8b235ba0d71299edaeec2742ea2f534f487f959b
Bozidar Dautovic <bozidardautovic@gmail.com>
2017-01-26 18:42:33 +0100

    Add new test for hanoi towers problem.

--- hanoi/tests.js
+++ hanoi/tests.js
@@ -18,4 +18,17 @@ describe('hanoi tower test', () => {
 
         c.should.deep.equal([3, 2, 1]);
     });
+
+    it('should work for input array with 10 elements', () => {
+        const a = [];
+        const b = [];
+        const c = [];
+        for (let i = 10; i >= 1; i--) {
+            a.push(i);
+        }
+
+        hanoi(a.length, a, b, c);
+
+        c.should.deep.equal([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+    });
 });
